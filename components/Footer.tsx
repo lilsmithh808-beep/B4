ea3ba779cdd392d6b6337065b84275e0a8102c0e
lilsmithh808-beep <lilsmithh808@gmail.com>
2026-07-31 04:56:53 +0100

    Add Footer component with branding and description

---
+++ components/Footer.tsx
@@ -0,0 +1,46 @@
+export default function Footer(){
+
+return (
+
+<footer className="
+bg-black
+text-white
+py-12
+px-6
+">
+
+
+<div className="
+max-w-7xl
+mx-auto
+">
+
+<h2 className="
+text-2xl
+font-bold
+text-pink-400
+">
+
+Narcos Bay
+
+</h2>
+
+
+<p className="
+text-gray-400
+mt-3
+">
+
+Premium shopping platform.
+
+</p>
+
+
+</div>
+
+
+</footer>
+
+);
+
+}
